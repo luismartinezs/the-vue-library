d20b05a2fffecafdc23d1fe7e7ff7c9302f6af13
Luis Martinez <luis@passionatepeople.io>
2021-12-10 15:00:40 +0100

    Paragraph with dropcap

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,3 @@
-<script setup>
-import { ref } from 'vue'
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import RetroTitle from "@/components/RetroTitle.vue";
-import RetroSubtitle from "@/components/RetroSubtitle.vue";
-import BackgroundWrapper from "@/components/BackgroundWrapper.vue";
-
-const bgOptions = ref({
-  type: 'retro',
-  lines: true,
-  grid: true
-})
-</script>
 
 <template>
   <div class="content">
@@ -19,8 +5,33 @@ const bgOptions = ref({
       <RetroTitle title="RETRO" star />
       <RetroSubtitle subtitle="Cool effect" />
     </BackgroundWrapper>
+    <div class="content-wrapper">
+      <VParagraph
+        dropcaps
+        text="Drop cap or dropped capital is a large capital letter used as a decorative element at the beginning of a paragraph or section. The size of a drop cap is usually two or more lines."
+      />
+      <VParagraph
+        text="The practice of using a large letter to mark the start of a text has been around for almost two thousand years. Illustrated caps increased usability by marking important passages and guiding readers through the text."
+      />
+    </div>
   </div>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import RetroTitle from "@/components/RetroTitle.vue";
+import RetroSubtitle from "@/components/RetroSubtitle.vue";
+import BackgroundWrapper from "@/components/BackgroundWrapper.vue";
+import VParagraph from '@/components/VParagraph.vue';
+
+const bgOptions = ref({
+  type: 'retro',
+  lines: true,
+  grid: true
+});
+</script>
 
 <style lang="scss">
 #app {
@@ -38,6 +49,9 @@ const bgOptions = ref({
       height: 30rem;
       border: 1px solid black;
       border-radius: 1rem;
+    }
+    &-wrapper {
+      padding: 2rem;
     }
   }
 }
---
+++ src/components/VParagraph.vue
@@ -0,0 +1,39 @@
+<template>
+  <p class="paragraph" :class="{ 'dropcaps': dropcaps }">{{ text }}</p>
+</template>
+
+<script setup>
+const props = defineProps({
+  dropcaps: {
+    type: Boolean,
+    default: false
+  },
+  text: {
+    type: String,
+    default: ''
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+.paragraph {
+  padding-bottom: 2rem;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+
+  line-height: 1.5;
+  letter-spacing: 0.3px;
+  word-spacing: 2px;
+  &.dropcaps {
+    &:first-child::first-letter {
+      color: #fe5f55;
+      font-weight: bold;
+      font-size: 70px;
+      float: left;
+      line-height: 60px;
+      padding-right: 8px;
+      margin-top: -3px;
+    }
+  }
+}
+</style>
